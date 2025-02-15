bfd8a1a3e5e2002d2768a9e06c1001bd023da214
Roohollah <m.roohollah.v@gmail.com>
2025-02-15 20:41:20 +0330

    set margin for sidebar title

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 		margin: 0;
 		--items-gap: 8px;
 		--items-color: rgb(105, 105, 105);
-        font-family: Roboto;
+		font-family: Roboto;
 	}
 	.container {
 		background-color: #3a3a3a;
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -11,19 +11,19 @@
 	}
 
 	const items = [
-		{ title: 'name1', count: 13, timestamp: 1739548594 },
-		{ title: 'name2', count: 3, timestamp: 1739548594 },
-		{ title: 'name3', count: 80, timestamp: 1739548594 },
-		{ title: 'name4', count: 6, timestamp: 1739548594 },
-		{ title: 'name5', count: 9, timestamp: 1739548594 },
-		{ title: 'name6', count: 8, timestamp: 1739548594 },
-		{ title: 'name7', count: 7, timestamp: 1739548594 },
-		{ title: 'name8', count: 7, timestamp: 1739548594 },
-		{ title: 'name9', count: 7, timestamp: 1739548594 },
-		{ title: 'name10', count: 7, timestamp: 1739548594 },
-		{ title: 'name11', count: 7, timestamp: 1739548594 },
-		{ title: 'name12', count: 7, timestamp: 1739548594 },
-		{ title: 'name13', count: 7, timestamp: 1739548594 }
+		{ title: 'Title name 1', count: 13, timestamp: 1739548594 },
+		{ title: 'Title name 2', count: 3, timestamp: 1739548594 },
+		{ title: 'Title name 3', count: 80, timestamp: 1739548594 },
+		{ title: 'Title name 4', count: 6, timestamp: 1739548594 },
+		{ title: 'Title name 5', count: 9, timestamp: 1739548594 },
+		{ title: 'Title name 6', count: 8, timestamp: 1739548594 },
+		{ title: 'Title name 7', count: 7, timestamp: 1739548594 },
+		{ title: 'Title name 8', count: 7, timestamp: 1739548594 },
+		{ title: 'Title name 9', count: 7, timestamp: 1739548594 },
+		{ title: 'Title name 10', count: 7, timestamp: 1739548594 },
+		{ title: 'Title name 11', count: 7, timestamp: 1739548594 },
+		{ title: 'Title name 12', count: 7, timestamp: 1739548594 },
+		{ title: 'Title name 13', count: 7, timestamp: 1739548594 }
 	];
 
 	let showSidebar = false;
@@ -52,7 +52,7 @@
 </div>
 {#if showSidebar}
 	<div class="sidebar" transition:fly={{ y: 200, duration: 200 }}>
-		<h3 style="bold;">{selectedTitle}</h3>
+		<h3 class="sidebar-title" style="bold" >{selectedTitle}</h3>
 	</div>
 {/if}
 
@@ -90,5 +90,9 @@
 		right: 0;
 		height: 200px;
 		animation: all 0.2s;
+	}
+
+	.sidebar-title {
+		margin: 10px;
 	}
 </style>
